c7f5a5d48cda9342898626bf53a2d66d9600cea4
Laura Darlak <laura@polyporedesign.com>
2018-11-04 21:48:02 +0000

    create login and logout routes for user authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,4 +18,29 @@ class UsersController < ApplicationController
     end
   end
 
+  get '/login' do
+    if !logged_in?
+      erb :'users/login'
+    else redirect '/'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    # if user != nil
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/'
+    else redirect '/signup'
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.destroy
+      redirect '/login'
+    else redirect '/'
+    end
+  end
+
 end
