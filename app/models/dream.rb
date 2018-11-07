6eef9c038bcf7394e755c532ed7ee90fb9649d62
Laura Darlak <laura@polyporedesign.com>
2018-11-07 18:44:58 +0000

    Add model validations and sort dream index by descending date

--- app/models/dream.rb
+++ app/models/dream.rb
@@ -5,6 +5,8 @@ class Dream < ActiveRecord::Base
   has_many :dream_themes
   has_many :themes, through: :dream_themes
 
+  validates :name, uniqueness: { case_sensitive: false }
+
   extend Slugger::ClassMethods
   include Slugger::InstanceMethods
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  validates :username, uniqueness: { case_sensitive: false }
+  validates :email, uniqueness: { case_sensitive: false }
+
   extend Slugger::ClassMethods
   include Slugger::InstanceMethods
 end
