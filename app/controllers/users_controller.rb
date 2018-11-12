0b04c9f561f88411a4add50eabf0a6aeffa25bd9
Laura Darlak <laura@polyporedesign.com>
2018-11-12 21:49:36 +0000

    Add links to themes on show dream view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,7 +49,7 @@ class UsersController < ApplicationController
   get '/logout' do
     if logged_in?
       session.destroy
-      redirect '/login'
+      redirect '/'
     else redirect '/'
     end
   end
