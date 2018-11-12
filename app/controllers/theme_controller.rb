5f814cbea773dc57cafcdc5a8f1b6054c06ae1fb
Laura Darlak <laura@polyporedesign.com>
2018-11-12 19:17:23 +0000

    Refactor collab method--modified array scope

--- app/controllers/theme_controller.rb
+++ app/controllers/theme_controller.rb
@@ -29,15 +29,15 @@ class ThemeController < ApplicationController
 
   helpers do
     def collab
-      @all_themes = []
+      all_themes = []
       current_user.dreams.map do |dream|
         if dream.themes.include?(@theme)
           dream.themes.map do |theme|
-              @all_themes << theme.name
+              all_themes << theme.name
           end
         end
       end
-      corrected_themes = @all_themes.uniq.reject{|theme| theme == @theme.name }
+      corrected_themes = all_themes.uniq.reject{|theme| theme == @theme.name }
       oxford_comma(corrected_themes)
     end
 
